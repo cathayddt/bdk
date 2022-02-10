ea83078f4ca41b25fc34595cf379994fa36d3671
kth-tw <88102592+kth-tw@users.noreply.github.com>
2022-02-10 06:30:26 +0000

    unit test: channel join

--- test/service/channel.test.ts
+++ test/service/channel.test.ts
@@ -7,6 +7,7 @@ import { NetworkCreateType } from '../../src/model/type/network.type'
 import Channel from '../../src/service/channel'
 import { PolicyTypeEnum } from '../../src/model/type/channel.type'
 import MinimumNetwork from '../util/minimumNetwork'
+import { DockerResultType } from '../../src/instance/infra/InfraRunner.interface'
 
 describe('Channel service:', function () {
   this.timeout(60000)
@@ -94,6 +95,73 @@ describe('Channel service:', function () {
           orderer: minimumNetwork.getOrderer().fullUrl,
         })
         assert.deepStrictEqual(fs.existsSync(`${config.infraConfig.bdkPath}/${config.networkName}/channel-artifacts/${channelName}/${channelName}.block`), true)
+      })
+    })
+  })
+
+  describe('join', () => {
+    it('should use joinSteps', async () => {
+      const channelJoinStepFetchChannelBlockStub = sinon.stub().resolves()
+      const channelJoinStepJoinOnInstanceStub = sinon.stub().resolves()
+      const channelJoinStepsStub = sinon.stub(Channel.prototype, 'joinSteps').callsFake(() => ({
+        fetchChannelBlock: channelJoinStepFetchChannelBlockStub,
+        joinOnInstance: channelJoinStepJoinOnInstanceStub,
+      }))
+      await channelService.join({
+        channelName: minimumNetwork.channelName,
+        orderer: minimumNetwork.getOrderer().fullUrl,
+      })
+      assert.deepStrictEqual(channelJoinStepFetchChannelBlockStub.called, true)
+      assert.deepStrictEqual(channelJoinStepJoinOnInstanceStub.called, true)
+      channelJoinStepsStub.restore()
+    })
+  })
+
+  describe('joinSteps', () => {
+    let channelName: string
+
+    before(async () => {
+      await minimumNetwork.createNetwork()
+      await minimumNetwork.peerAndOrdererUp()
+      channelName = minimumNetwork.channelName
+      await channelServiceOrg0Peer.create({
+        channelName,
+        orgNames: [networkCreateJson.peerOrgs[0].name],
+        channelAdminPolicy: { type: PolicyTypeEnum.IMPLICITMETA, value: 'ANY Admins' },
+        lifecycleEndorsement: { type: PolicyTypeEnum.IMPLICITMETA, value: 'ANY Endorsement' },
+        endorsement: { type: PolicyTypeEnum.IMPLICITMETA, value: 'ANY Endorsement' },
+        orderer: minimumNetwork.getOrderer().fullUrl,
+      })
+    })
+
+    after(async () => {
+      await minimumNetwork.deleteNetwork()
+    })
+
+    describe('fetchChannelBlock', () => {
+      it('should fetch channel block', async () => {
+        await channelServiceOrg0Peer.joinSteps().fetchChannelBlock({
+          channelName,
+          orderer: minimumNetwork.getOrderer().fullUrl,
+        })
+        assert.strictEqual(fs.existsSync(`${config.infraConfig.bdkPath}/${config.networkName}/channel-artifacts/${channelName}/${channelName}.tx`), true)
+      })
+    })
+
+    describe('joinOnInstance', () => {
+      before(async () => {
+        await channelServiceOrg0Peer.joinSteps().fetchChannelBlock({
+          channelName,
+          orderer: minimumNetwork.getOrderer().fullUrl,
+        })
+      })
+
+      it('should join channel on instance', async () => {
+        await channelServiceOrg0Peer.joinSteps().joinOnInstance({
+          channelName,
+          orderer: minimumNetwork.getOrderer().fullUrl,
+        })
+        assert.deepStrictEqual(Channel.parser.listJoinedChannel((await channelServiceOrg0Peer.listJoinedChannel() as DockerResultType)), [minimumNetwork.channelName])
       })
     })
   })
