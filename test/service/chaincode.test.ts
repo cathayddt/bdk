36f314648a0805d6e620543d018b134788315f06
kth-tw <88102592+kth-tw@users.noreply.github.com>
2022-02-21 03:18:12 +0000

    unit test: chaincode package

--- test/service/chaincode.test.ts
+++ test/service/chaincode.test.ts
@@ -1,4 +1,6 @@
 /* global describe, it, before, after, beforeEach, afterEach */
+import fs from 'fs'
+import assert from 'assert'
 import config from '../../src/config'
 import Chaincode from '../../src/service/chaincode'
 import MinimumNetwork from '../util/minimumNetwork'
@@ -16,8 +18,16 @@ describe('Chaincode service:', function () {
     chaincodeServiceOrg0Peer = new Chaincode(minimumNetwork.org0PeerConfig)
   })
 
-  // describe('package', () => {
-  // })
+  describe('package', () => {
+    it('should package chaincode', async () => {
+      await chaincodeService.package({
+        name: 'fabcar',
+        version: 1,
+        path: './chaincode/fabcar/go',
+      })
+      assert.strictEqual(fs.existsSync(`${config.infraConfig.bdkPath}/${config.networkName}/chaincode/fabcar_1.tar.gz`), true)
+    })
+  })
 
   // describe('invoke', () => {
   // })
