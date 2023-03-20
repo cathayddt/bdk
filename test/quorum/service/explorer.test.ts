a52b605f6cd1654f0fc4fb883b41f02cd6c31a47
kidneyweak <kidneyweak@gmail.com>
2023-03-20 16:48:59 +0800

    test: unit test quorum explorer 1. add quorum explorer service unit test 2. grep Quorum to test coverage 3. edit `.nycrc.json` to coverage quorum service folder Tickets: CU-8669uqjeg

---
+++ test/quorum/service/explorer.test.ts
@@ -0,0 +1,51 @@
+/* global describe, it */
+import Dockerode from 'dockerode'
+import assert from 'assert'
+import Network from '../../../src/quorum/service/network'
+import Explorer from '../../../src/quorum/service/explorer'
+import config from '../../../src/quorum/config'
+
+describe('Quorum.Explorer', function () {
+  this.timeout(60000)
+
+  const docker: Dockerode = new Dockerode({ socketPath: '/var/run/docker.sock' })
+  const dockerdOption = { all: true }
+  const network = new Network(config)
+  const explorer = new Explorer(config)
+  const port = 9000
+
+  describe('Quorum.Explorer.create', () => {
+    it('should create and start the explorer', async () => {
+      const initContainers = await docker.listContainers(dockerdOption)
+      await network.createBdkFolder()
+      await explorer.create(port)
+      const upContainers = await docker.listContainers(dockerdOption)
+
+      assert.strictEqual(upContainers.length, initContainers.length + 2)
+      const explorerContainer = upContainers.find(container => container.Names.some(name => name.includes('blockscout')))
+
+      const containerInfo = await docker.getContainer(explorerContainer?.Id as string).inspect()
+      assert.strictEqual(containerInfo.State.Status as string, 'running', 'Container is not running')
+    })
+  })
+
+  describe('Quorum.Explorer.getExplorerFiles', () => {
+    // Populate the files as needed for testing purposes
+    it('should return a list of explorer files', () => {
+      const explorerFiles = explorer.getExplorerFiles()
+      assert(Array.isArray(explorerFiles))
+      assert.strictEqual(explorerFiles[0], 'explorer-docker-compose.yaml')
+    })
+  })
+
+  describe('Quorum.Explorer.delete', () => {
+    it('should delete the explorer', async () => {
+      await explorer.delete()
+
+      const containers = await docker.listContainers(dockerdOption)
+      const explorerContainer = containers.find(container => container.Names.some(name => name.includes('blockscout')))
+
+      assert(!explorerContainer)
+    })
+  })
+})
