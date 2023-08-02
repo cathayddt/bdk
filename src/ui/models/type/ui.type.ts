8201b656ab3d37b8324e3bbcdfc783fc6d4ba721
kidneyweak <kidneyweak@gmail.com>
2023-08-02 16:09:25 +0800

    feat: select and excute command

---
+++ src/ui/models/type/ui.type.ts
@@ -0,0 +1,8 @@
+export interface ItemProps {
+  label: string
+  value: string
+}
+
+export interface CommandProps {
+  type: string
+}
--- src/ui/views/app.tsx
+++ src/ui/views/app.tsx
@@ -1,20 +1,17 @@
 import React, { useState } from 'react'
-import { Box, Text, useApp, useInput, useStdin } from 'ink'
+import { Box, Text, useApp, useInput } from 'ink'
 import Logo from './logo'
 import Command from './command'
 import DockerLogs from './dockerLogs'
 import Terminal from './terminal'
 import SelectInput from 'ink-select-input'
+import CommandContext from '../services/commandContext'
 
 export default function App () {
   const { exit } = useApp()
-  const [type, setType] = useState('Fabric')
-  useInput((input, key) => {
-    if (input === 'q' || (key.ctrl && input === 'c') || key.escape) {
-      exit()
-    }
-  })
-  const items = [
+  const commandContext = new CommandContext()
+  const [type, setType] = useState('bdk fabric')
+  const [items, setItems] = useState([
     {
       label: 'Fabric',
       value: 'bdk fabric',
@@ -23,21 +20,34 @@ export default function App () {
       label: 'Quorum',
       value: 'bdk quorum',
     },
-  ]
+  ])
+  useInput((input, key) => {
+    if (input === 'q' || (key.ctrl && input === 'c') || key.escape) {
+      exit()
+    }
+  })
+
   const selectChain = (item:any) => {
-    setType(item.label)
+    setType(item.value)
+  }
+
+  const handleCommand = (item: any) => {
+    const commandList = commandContext.makeItem(item.value)
+    if (commandList.length === 0) {
+      commandContext.executeCommand(item.value)
+    } else setItems(commandList)
   }
 
   return (
     <Box borderStyle='single' flexDirection='column' height={50}>
       <Box flexDirection='row' height={90}>
-        <Box flexDirection='column' flexGrow={1}>
+        <Box flexDirection='column' width="50%">
           <Logo />
           <Command />
-          <SelectInput items={items} onHighlight={selectChain}/>
+          <SelectInput items={items} onHighlight={selectChain} onSelect={handleCommand}/>
           <DockerLogs />
         </Box>
-        <Box flexGrow={1}>
+        <Box width="50%">
           <Terminal type={type}/>
         </Box>
       </Box>
