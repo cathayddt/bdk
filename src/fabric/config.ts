55eb8952ab90062186816e4966245c9d31314d22
casper900212 <casper900212@gmail.com>
2025-05-19 10:07:15 +0800

    chore: upgrade fabric versoin to 2.5.1 upgrade fabric version to 2.5.1

--- src/fabric/config.ts
+++ src/fabric/config.ts
@@ -90,9 +90,9 @@ const config: Config = {
   orgDomainName: process.env.BDK_ORG_DOMAIN || '',
   hostname: process.env.BDK_HOSTNAME || '',
   fabricVersion: {
-    orderer: process.env.FABRIC_ORDERER_VERSION || '2.3.1',
-    peer: process.env.FABRIC_PEER_VERSION || '2.3.1',
-    tools: process.env.FABRIC_TOOLS_VERSION || '2.3.1',
+    orderer: process.env.FABRIC_ORDERER_VERSION || '2.5.1',
+    peer: process.env.FABRIC_PEER_VERSION || '2.5.1',
+    tools: process.env.FABRIC_TOOLS_VERSION || '2.5.1',
     ca: process.env.FABRIC_CA_VERSION || '1.5.0',
     explorer: '1.1.3',
     explorerDb: '1.1.3',
